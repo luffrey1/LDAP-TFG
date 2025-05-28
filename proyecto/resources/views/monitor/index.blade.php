c5324ec2f465beaad09b031d91205a71db167bd3
luffrey1 <luffrey2@gmail.com>
2025-05-28 11:20:24 +0200

    añadido boton borrar host de nuevo

--- proyecto/resources/views/monitor/index.blade.php
+++ proyecto/resources/views/monitor/index.blade.php
@@ -298,10 +298,16 @@
             });
         });
     });
-    
+
+    // Definir eliminarHost en el scope global
     function eliminarHost(id, nombre) {
+        var form = document.getElementById('form-eliminar-' + id);
+        if (!form) {
+            alert('No se encontró el formulario para eliminar el host.');
+            return;
+        }
         if (confirm('¿Está seguro que desea eliminar el equipo "' + nombre + '"? Esta acción no se puede deshacer.')) {
-            document.getElementById('form-eliminar-' + id).submit();
+            form.submit();
         }
     }
 </script>
