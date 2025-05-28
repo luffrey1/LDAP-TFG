9c7ffd63022c6122d4206f1d410d328bf93bee5b
luffrey1 <luffrey2@gmail.com>
2025-05-28 10:46:41 +0200

    api.php limpio

--- proyecto/routes/api.php
+++ proyecto/routes/api.php
@@ -18,11 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Rutas para el terminal SSH
-Route::prefix('terminal')->middleware('auth:sanctum')->group(function () {
-    Route::post('/connect', [App\Http\Controllers\SshTerminalController::class, 'connect']);
-    Route::post('/send', [App\Http\Controllers\SshTerminalController::class, 'execute']);
-    Route::post('/disconnect', [App\Http\Controllers\SshTerminalController::class, 'disconnect']);
-});
 
 Route::post('/telemetry/update', [\App\Http\Controllers\MonitorController::class, 'updateTelemetry']); 
