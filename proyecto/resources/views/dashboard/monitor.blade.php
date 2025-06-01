594201d100cf93e55833803a19278039cdc8f2bd
luffrey1 <tu_email@ejemplo.com>
2025-06-01 16:05:23 +0200

    arreglando colores...

--- proyecto/resources/views/dashboard/monitor.blade.php
+++ proyecto/resources/views/dashboard/monitor.blade.php
@@ -28,7 +28,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Equipos</div>
+                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">Total Equipos</div>
                             <div class="h5 mb-0 font-weight-bold text-white counter" id="total-hosts">{{ count($hosts) }}</div>
                         </div>
                         <div class="col-auto">  
