142d81f989402d5a7011e686f69baf3fc09d7ecf
luffrey1 <tu_email@ejemplo.com>
2025-06-05 22:18:13 +0200

    ssh

--- webssh2/app/server/util.js
+++ webssh2/app/server/util.js
@@ -4,69 +4,44 @@
 // private
 const debug = require('debug')('WebSSH2');
 const Auth = require('basic-auth');
-const config = require('./config');
 
 let defaultCredentials = { username: null, password: null, privatekey: null };
 
-function setDefaultCredentials(user) {
-  if (!user) return;
-  
-  if (user.name) {
-    config.ssh.username = user.name;
-  }
-  if (user.password) {
-    config.ssh.password = user.password;
-  }
-  if (user.privatekey) {
-    config.ssh.privateKey = user.privatekey;
-  }
-}
+exports.setDefaultCredentials = function setDefaultCredentials({
+  name: username,
+  password,
+  privatekey,
+  overridebasic,
+}) {
+  defaultCredentials = { username, password, privatekey, overridebasic };
+};
 
-function basicAuth(req, res, next) {
-  // Si no hay configuración de usuario, permitir acceso
-  if (!config.user || (!config.user.name && !config.user.password && !config.user.privatekey)) {
-    return next();
+exports.basicAuth = function basicAuth(req, res, next) {
+  const myAuth = Auth(req);
+  // If Authorize: Basic header exists and the password isn't blank
+  // AND config.user.overridebasic is false, extract basic credentials
+  // from client]
+  const { username, password, privatekey, overridebasic } = defaultCredentials;
+  if (myAuth && myAuth.pass !== '' && !overridebasic) {
+    req.session.username = myAuth.name;
+    req.session.userpassword = myAuth.pass;
+    debug(`myAuth.name: ${myAuth.name} and password ${myAuth.pass ? 'exists' : 'is blank'}`);
+  } else {
+    req.session.username = username;
+    req.session.userpassword = password;
+    req.session.privatekey = privatekey;
   }
-
-  // Si overridebasic es true, permitir acceso
-  if (config.user.overridebasic) {
-    return next();
+  if (!req.session.userpassword && !req.session.privatekey) {
+    res.statusCode = 401;
+    debug('basicAuth credential request (401)');
+    res.setHeader('WWW-Authenticate', 'Basic realm="WebSSH"');
+    res.end('Username and password required for web SSH service.');
+    return;
   }
-
-  // Verificar autenticación básica
-  const auth = req.headers.authorization;
-  if (!auth) {
-    res.setHeader('WWW-Authenticate', 'Basic realm="WebSSH2"');
-    return res.status(401).send('Authentication required');
-  }
-
-  const [type, credentials] = auth.split(' ');
-  if (type !== 'Basic') {
-    return res.status(401).send('Invalid authentication type');
-  }
-
-  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':');
-  
-  // Verificar credenciales
-  if (username === config.user.name && password === config.user.password) {
-    // Establecer las credenciales en la sesión
-    if (req.session) {
-      req.session.username = username;
-      req.session.userpassword = password;
-    }
-    return next();
-  }
-
-  res.setHeader('WWW-Authenticate', 'Basic realm="WebSSH2"');
-  return res.status(401).send('Invalid credentials');
-}
+  next();
+};
 
 // takes a string, makes it boolean (true if the string is true, false otherwise)
 exports.parseBool = function parseBool(str) {
   return str.toLowerCase() === 'true';
-};
-
-module.exports = {
-  setDefaultCredentials,
-  basicAuth
 };
